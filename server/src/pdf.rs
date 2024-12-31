d49ecfbff59a56a5b4358aa2cc2b2248d967f62c
Proxtx <gopro54122@gmail.com>
2024-12-31 03:37:48 +0100

    compare pdf pages and find twins 💕

--- server/src/pdf.rs
+++ server/src/pdf.rs
@@ -1,6 +1,7 @@
 use image::RgbImage;
 use pdfium_render::prelude::*;
 use rayon::prelude::*;
+use std::sync::atomic::AtomicUsize;
 use std::sync::Arc;
 use std::thread::JoinHandle;
 use std::{path::Path, thread};
@@ -9,6 +10,29 @@ use std::{path::Path, thread};
 enum Comparison {
     Identical,
     Different(Vec<usize>),
+}
+
+#[derive(Debug)]
+enum Similiarity {
+    Different,
+    Similar(usize),
+}
+
+impl Similiarity {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match (self, other) {
+            (Similiarity::Different, Similiarity::Different) => std::cmp::Ordering::Equal,
+            (Similiarity::Different, Similiarity::Similar(_)) => std::cmp::Ordering::Greater,
+            (Similiarity::Similar(_), Similiarity::Different) => std::cmp::Ordering::Less,
+            (Similiarity::Similar(s), Similiarity::Similar(o)) => s.cmp(o),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum PageSimilarity {
+    Different,
+    Similar(usize, usize),
 }
 
 #[derive(Debug)]
@@ -50,18 +74,59 @@ impl PDFComparison {
             //let handle_a = s.spawn(|| self.extract_images(pdf_a));
             //let handle_b = s.spawn(|| self.extract_images(pdf_b));
             //(handle_a.join().unwrap(), handle_b.join().unwrap())
-            self.extract_images(pdf_a.clone());
-            println!("a worked");
             (self.extract_images(pdf_a), self.extract_images(pdf_b))
         });
 
-        return Ok(Vec::new());
         let (images_a, images_b) = match images {
             (Ok(images_a), Ok(images_b)) => (images_a, images_b),
             (Err(e), _) | (_, Err(e)) => return Err(e),
         };
 
+        let page_similarities = PDFComparison::find_min_similarity_for_images(&images_a, &images_b);
+
+        println!("{:?}", page_similarities);
+
         Ok(Vec::new())
+    }
+
+    fn find_min_similarity_for_images(
+        img_a: &Vec<RgbImage>,
+        img_b: &Vec<RgbImage>,
+    ) -> Vec<PageSimilarity> {
+        img_a
+            .par_iter()
+            .map(|a| PDFComparison::find_min_similarity(a, img_b))
+            .collect()
+    }
+
+    fn find_min_similarity(img_a: &RgbImage, img_b: &Vec<RgbImage>) -> PageSimilarity {
+        match img_b
+            .par_iter()
+            .enumerate()
+            .map(|(i, v)| (i, PDFComparison::compare_images(img_a, v)))
+            .min_by(|a, b| a.1.cmp(&b.1))
+        {
+            Some((i, sim)) => match sim {
+                Similiarity::Similar(sim) => PageSimilarity::Similar(i, sim),
+                Similiarity::Different => PageSimilarity::Different,
+            },
+            None => PageSimilarity::Different,
+        }
+    }
+
+    fn compare_images(img_a: &RgbImage, img_b: &RgbImage) -> Similiarity {
+        let similarity = AtomicUsize::new(0);
+        if img_a.dimensions() != img_b.dimensions() {
+            return Similiarity::Different;
+        }
+        (0..img_a.dimensions().0).into_par_iter().for_each(|x| {
+            (0..img_a.dimensions().1).into_par_iter().for_each(|y| {
+                if img_a.get_pixel(x, y) != img_b.get_pixel(x, y) {
+                    similarity.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+                }
+            })
+        });
+        Similiarity::Similar(similarity.into_inner())
     }
 
     fn extract_images(&self, pdf: Arc<PdfDocument>) -> Result<Vec<RgbImage>, PDFComparisonError> {
@@ -71,9 +136,7 @@ impl PDFComparison {
             .rotate_if_landscape(PdfPageRenderRotation::Degrees90, true);
 
         (0..pdf.pages().len())
-            .into_iter()
             .map(|p_index| {
-                println!("At p index: {}", p_index);
                 match pdf
                     .pages()
                     .get(p_index)
